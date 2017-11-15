267111166f7d2e3cb8e6700bfc61f477a441400f
odoyie <33414676+odoyie@users.noreply.github.com>
2017-11-15 11:51:01 +0700

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -86,7 +86,7 @@ public class CameraActivity extends Fragment {
             frameContainerLayout.setLayoutParams(layoutParams);
 
             //video view
-            mPreview = new Preview(getActivity(), mCamera);
+            mPreview = new Preview(getActivity());
             FrameLayout mainLayout = (FrameLayout) view.findViewById(getResources().getIdentifier("video_view", "id", appResourcesPackage));
             mainLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
             mainLayout.addView(mPreview);
